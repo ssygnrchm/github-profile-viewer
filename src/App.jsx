506dfd2907698810bb361b38b6aa9341b902b850
Sisy Agnia <45039754+ssygnrchm@users.noreply.github.com>
2025-08-03 12:06:33 +0000

    change to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// CHANGE THIS LINE:
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import UserProfilePage from './components/UserProfilePage';
 import RepoDetailsPage from './components/RepoDetailsPage';
-import NotFoundPage from './components/NotFoundPage'; // New import
+import NotFoundPage from './components/NotFoundPage';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router> {/* This is now HashRouter */}
       <div className="app-main-container">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/user/:username" element={<UserProfilePage />} />
           <Route path="/user/:username/:repoName" element={<RepoDetailsPage />} />
-          <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route for 404 */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
